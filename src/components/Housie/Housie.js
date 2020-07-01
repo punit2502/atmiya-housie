c71de9831404399cc64e3b5eb0b9f0cb9d2df01b
punit2502 <punit.makwana25@gmail.com>
2020-07-01 17:13:26 +0530

    change to 24 hours clock

--- src/components/Housie/Housie.js
+++ src/components/Housie/Housie.js
@@ -13,13 +13,10 @@ const formatDate = (date = new Date()) => {
   const day = date.getDate().toString().padStart(2, "0");
   const month = date.getMonth().toString().padStart(2, "0");
   const year = date.getFullYear();
-  let hours = date.getHours();
+  const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
-  const ampm = hours >= 12 ? "PM" : "AM";
 
-  hours = hours.toString().padStart(2, "0");
-
-  return `${day}/${month}/${year} ${hours}:${minutes} ${ampm}`;
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 };
 
 const handleDownload = () => {
